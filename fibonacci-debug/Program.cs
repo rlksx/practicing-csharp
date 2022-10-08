618186322e31a62188e6e40ff88d909ac2a05e9a
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-10-08 00:17:16 -0300

    add-fibonacci-debug

---
+++ fibonacci-debug/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace fibonacci_debug;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var fibonacciNumbers = new List<int> { 1, 1 };
+
+        while (fibonacciNumbers.Count < 10)
+        {
+            var previus01 = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previus02 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previus01 + previus02);
+        }
+
+        foreach (var item in fibonacciNumbers)
+            Console.WriteLine(item);
+    }
+}
